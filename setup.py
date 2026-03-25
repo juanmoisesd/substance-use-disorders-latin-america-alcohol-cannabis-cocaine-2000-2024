29724e0edb9a92384e9af648261cbfd90603f9d9
Juan Moises de la Serna <jmsernatuya@gmail.com>
2026-03-25 09:48:04 +0100

    Add/update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,2 @@
-from setuptools import setup, find_packages
-setup(
-    name="substance-use-disorders-latin-america-alcohol-cannabis-cocaine-2000-2024",
-    version="1.0.0",
-    description="Substance use disorders database for 14 Latin American countries (2000–2024): alcohol, cannabis, coc",
-    author="de la Serna, Juan Moisés",
-    url="https://github.com/juanmoisesd/substance-use-disorders-latin-america-alcohol-cannabis-cocaine-2000-2024",
-    packages=find_packages(),
-    install_requires=["pandas>=1.3.0","requests>=2.26.0"],
-    python_requires=">=3.7",
-    classifiers=["Programming Language :: Python :: 3","License :: OSI Approved :: MIT License","Topic :: Scientific/Engineering"],
-    keywords="zenodo, open-data",
-)
+from setuptools import setup,find_packages
+setup(name="substance-use-disorders-latin-america-alcohol-cannabis-cocaine-2000-2024",version="1.0.0",description="Substance use disorders database for 14 Latin American countries (2000–2024): alcohol, cannabis, coc" ,author="de la Serna, Juan Moisés",url="https://github.com/juanmoisesd/substance-use-disorders-latin-america-alcohol-cannabis-cocaine-2000-2024",packages=find_packages(),install_requires=["pandas>=1.3.0","requests>=2.26.0"],python_requires=">=3.7",keywords="zenodo, open-data")
